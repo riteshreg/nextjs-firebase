f6cb2d9af09f68bf75808cddf98a48e712a9c747
riteshreg <webriteshkhadka@gmail.com>
2025-11-29 09:35:34 +0545

    feat: add PostCSS configuration and SVG assets
    - Created a PostCSS configuration file to integrate Tailwind CSS.
    - Added multiple SVG files for icons and graphics, including:
      - file.svg
      - globe.svg
      - next.svg
      - vercel.svg
      - window.svg
    - Introduced a TypeScript configuration file (tsconfig.json) with strict settings and Next.js support.

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xl text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            This Next.js app is deployed on Firebase Hosting.
+          </h1>
+          <p className="max-w-xl text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Done by Retech Labs.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
